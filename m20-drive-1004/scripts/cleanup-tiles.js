ef7470579e4cbc7dec605f3d8befa965cb5c0e50
garrett johnson <garrett.kjohnson@gmail.com>
2024-07-15 18:35:21 +0900

    Update script to not remove index images

--- m20-drive-1004/scripts/cleanup-tiles.js
+++ m20-drive-1004/scripts/cleanup-tiles.js
@@ -12,15 +12,8 @@ const __dirname = path.dirname( __filename );
 // cli arguments
 const { directory = 'tiles' } = yargs( process.argv ).argv;
 
-// remove unused images and json files
+// remove unused json files
 const tilesPath = path.resolve( __dirname, `../${ directory }` );
-globSync( path.resolve( tilesPath, '**/*_index.png' ).replace( /\\/g, '/' ) )
-    .forEach( p => {
-
-        fs.rmSync( p );
-
-    } );
-
 globSync( path.resolve( tilesPath, '**/*_message.json' ).replace( /\\/g, '/' ) )
     .forEach( p => {
 
